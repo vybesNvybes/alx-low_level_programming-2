09e62c74f2c0980361b06a360592c4607deca53d
taymhieCraay <ekundayotimi2018@gmail.com>
2022-10-20 11:14:34 -0700

    Write a function that prints all the elements of a lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,12 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -14,15 +8,15 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
 
 typedef struct list_s
 {
 
-char *str;
-unsigned int len;
-struct list_s *next;
-
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
